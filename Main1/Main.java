acb0d7d139fbd6e7af092a9cb8093ac16fa09ae9
eduardosanch <eduardosanch2006@gmail.com>
2026-02-12 21:41:17 -0600

    Actividades week 1

---
+++ Main1/Main.java
@@ -0,0 +1,18 @@
+
+
+/**
+ * Class Main
+ */
+public class Main {
+
+
+  /**
+   * @param        args
+   */
+  public static void main(String[] args){
+	Persona yo = new Persona("eduardo", 19);
+	yo.saludar();
+  }
+
+
+}
